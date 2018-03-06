70e794453411272cb7cd93699b44a32347f129a3
MPopovych <vwotar@gmail.com>
2018-03-06 21:05:38 +0200

    Added google maps support

--- app/src/main/java/com/makki/robomanager/fragments/NavigationFragment.java
+++ app/src/main/java/com/makki/robomanager/fragments/NavigationFragment.java
@@ -1,18 +1,25 @@
 package com.makki.robomanager.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.makki.robomanager.R;
 
 /**
  * @author Max.Popovych on 05.03.18.
  */
 
-public class NavigationFragment extends Fragment {
+public class NavigationFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     public static NavigationFragment newInstance(){
         //TODO: --Use this to set arguments for the fragment
@@ -25,12 +32,21 @@ public class NavigationFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_navigation, container, false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_navigation, container, false);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment map = ((SupportMapFragment)getChildFragmentManager()
+                .findFragmentById(R.id.map_container));
+        map.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.e("TT", "SUCCESS?");
     }
 }
